0d4f846dec5b0b05a1a24320b6fb66997cc2921d
Stefan Walkner <stefan@arkulpa.at>
2018-12-20 20:38:23 +0100

    replace test code with iterator which runs all current solutions

--- run_all.py
+++ run_all.py
@@ -5,18 +5,10 @@ from collections import deque
 
 start = time.time()
 
-# for day in range(1, 10):
-#     day_string = 'day_{:02d}'.format(day)
-#     print(day_string)
-#     os.system('python3 {}_part_1.py'.format(day_string))
-#     os.system('python3 {}_part_2.py'.format(day_string))
-#
-# print('********* FULL TIME FOR ALL {:.2f} seconds'.format(time.time() - start))
+for day in range(1, 12):
+    day_string = 'day_{:02d}'.format(day)
+    print(day_string)
+    os.system('python3 {}_part_1.py'.format(day_string))
+    os.system('python3 {}_part_2.py'.format(day_string))
 
-
-testing = deque([1, 2, 3, 4, 5])
-print(testing)
-print(testing.rotate(1))
-print(testing)
-testing.append(6)
-print(testing)
+print('********* FULL TIME FOR ALL {:.2f} seconds'.format(time.time() - start))
